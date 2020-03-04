b9e6ab313304a135f368e3b007e251a1d896d9e1
vigarbuaa <vigarxueer@126.com>
2020-03-04 10:37:47 +0800

    add lombok

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ you can use the script `banchmark.py` in command line.
 ## demo
 
 https://mengkang.net/demo/websocket/2.html
+
+## 待加入特性
+   - 检查，报文只接收文本，且必须为json
+   - 根据报文的type字段，再次区别不同的handler
+   - 利用netty中的schedule，定时向各分系统推送
+   - 做一个输送系统模拟器，后续做自检用
--- src/main/java/net/mengkang/entity/Client.java
+++ src/main/java/net/mengkang/entity/Client.java
@@ -1,8 +1,11 @@
 package net.mengkang.entity;
 
+import lombok.Data;
+
 /**
  * Created by zhoumengkang on 16/7/2.
  */
+@Data
 public class Client {
     private long id;
     private int roomId;
@@ -10,21 +13,5 @@ public class Client {
     public Client() {
         id = 0L;
         roomId = 0;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public int getRoomId() {
-        return roomId;
-    }
-
-    public void setRoomId(int roomId) {
-        this.roomId = roomId;
     }
 }
